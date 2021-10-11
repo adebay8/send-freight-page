d0f132c03a59be945eacf90621937fdc3829165a
Adebayo Onuchukwu <adebayop.o@gmail.com>
2021-10-11 17:30:04 +0530

    work complete

--- src/containers/AdditionalServiceContainer.js
+++ src/containers/AdditionalServiceContainer.js
@@ -3,63 +3,49 @@ import "../App.scss";
 import Toggle from "../components/toggle";
 
 const AdditionalServiceContainer = () => {
-  const [toggleState, setToggleState] = useState(false);
   return (
     <section className="container">
       <div className="section-header" style={{ marginBottom: "30px" }}>
         <h4>Additional Services</h4>
       </div>
       <div className="additional-services">
-        <div className="flex">
-          <Toggle
-            toggleStatus={toggleState}
-            handleToggle={(e) => setToggleState(e)}
-          />
-          <div className="additional-service">
-            <p className="title">Export Forwarding</p>
-            <span className="subtitle">
-              We handle clearance and documentation
-            </span>
-          </div>
-        </div>
-        <div className="flex">
-          <Toggle
-            toggleStatus={toggleState}
-            handleToggle={(e) => setToggleState(e)}
-          />
-          <div className="additional-service">
-            <p className="title">Import Customs Clearance</p>
-            <span className="subtitle">
-              We handle import customs and regulatory requirements
-            </span>
-          </div>
-        </div>
-        <div className="flex">
-          <Toggle
-            toggleStatus={toggleState}
-            handleToggle={(e) => setToggleState(e)}
-          />
-          <div className="additional-service">
-            <p className="title">Cargo Insurance</p>
-            <span className="subtitle">
-              Protect your cargo from logistics risks up to full value
-            </span>
-          </div>
-        </div>
-        <div className="flex">
-          <Toggle
-            toggleStatus={toggleState}
-            handleToggle={(e) => setToggleState(e)}
-          />
-          <div className="additional-service">
-            <p className="title">Transport / Delivery</p>
-            <span className="subtitle">
-              We deliver the cargo to your door step from the ports
-            </span>
-          </div>
-        </div>
+        <AdditionalServiceContainer.Service
+          title="Export Forwarding"
+          subtitle="We handle clearance and documentation"
+        />
+        <AdditionalServiceContainer.Service
+          title="Import Customs Clearance"
+          subtitle="We handle import customs and regulatory requirements"
+        />
+        <AdditionalServiceContainer.Service
+          title="Cargo Insurance"
+          subtitle="Protect your cargo from logistics risks up to full value"
+        />
+        <AdditionalServiceContainer.Service
+          title="Transport / Delivery"
+          subtitle="We deliver the cargo to your door step from the ports"
+        />
       </div>
     </section>
+  );
+};
+
+AdditionalServiceContainer.Service = function AdditionalService({
+  title,
+  subtitle,
+}) {
+  const [toggleState, setToggleState] = useState(false);
+  return (
+    <div className="flex">
+      <Toggle
+        toggleStatus={toggleState}
+        handleToggle={(e) => setToggleState(e)}
+      />
+      <div className="additional-service">
+        <p className="title">{title}</p>
+        <span className="subtitle">{subtitle}</span>
+      </div>
+    </div>
   );
 };
 
--- src/containers/ServiceContainer.js
+++ src/containers/ServiceContainer.js
@@ -1,5 +1,7 @@
 import "../App.scss";
 import AppIcon from "../components/icons/Icon";
+import boat from "../assets/boat.png";
+import officer from "../assets/officer.png";
 
 const ServiceContainer = () => {
   return (
@@ -16,8 +18,8 @@ const ServiceContainer = () => {
         </div>
         <div className="service">
           <span className="service-title">Sea freight</span>
-          <div className="service-icon truck">
-            <AppIcon type="fa" name="truck" size={90} />
+          <div className="service-icon boat">
+            <img src={boat} alt="boat" />
           </div>
         </div>
         <div className="service">
@@ -36,8 +38,8 @@ const ServiceContainer = () => {
             <br />
             Clearance
           </span>
-          <div className="service-icon airplane">
-            <AppIcon type="md" name="ic_airplanemode_active" size={100} />
+          <div className="service-icon officer">
+            <img src={officer} alt="Officer" />
           </div>
         </div>
       </div>
